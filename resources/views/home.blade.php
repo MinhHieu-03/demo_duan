26bf3d1005c241377914d819eb12a6be6ee94e97
minhhieu899 <phungminhhieu899@gmail.com>
2025-04-07 21:01:56 +0700

    first

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -166,32 +166,6 @@
 });
 
 </script>
-<script>
-    toastr.options = {
-        "closeButton": true,
-        "progressBar": true,
-        "positionClass": "toast-top-right"
-    };
-
-    @if (session('success'))
-        toastr.success("{{ session('success') }}");
-    @endif
-
-    @if (session('error'))
-        toastr.error("{{ session('error') }}");
-    @endif
-
-    @if (session('info'))
-        toastr.info("{{ session('info') }}");
-    @endif
-
-    @if (session('warning'))
-        toastr.warning("{{ session('warning') }}");
-    @endif
-</script>
-
-
-
 
 <style>
         .row-product-section {
